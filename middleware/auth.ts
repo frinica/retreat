50203d8e0153acc9406175541a4a1e0fede9a20c
frinica <you@example.com>
2023-06-29 15:21:10 +0200

    update middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,3 +1,16 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  console.log("Hello from middleware!")
+import { storeToRefs } from "pinia"
+import { useAuthStore } from "store/auth"
+
+export default defineNuxtRouteMiddleware((to) => {
+  const { authenticated } = storeToRefs(useAuthStore())
+  const token = useCookie("token")
+
+  if (token.value && to?.name === "login") {
+    return navigateTo("/")
+  }
+
+  if (!token.value && to?.name !== "login") {
+    abortNavigation()
+    return navigateTo("/login")
+  }
 })
