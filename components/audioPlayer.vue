989775f1d9201c578fdd860cbcb67b34d109b1fa
frinica <you@example.com>
2023-07-10 13:59:43 +0200

    move audio tracks to composables

--- components/audioPlayer.vue
+++ components/audioPlayer.vue
@@ -1,14 +1,6 @@
 <script lang="ts" setup>
 import { ref } from "vue"
 import { useAuthStore } from "../store/auth"
-import fireplace from "~/assets/audio/audio-fireplace.mp3"
-import forestriver from "~/assets/audio/audio-forestriver.mp3"
-import nightambience from "~/assets/audio/audio-nightambience.mp3"
-import ocean from "~/assets/audio/audio-ocean.mp3"
-import softrain from "~/assets/audio/audio-softrain.mp3"
-import thunderstorm from "~/assets/audio/audio-thunderstorm.mp3"
-import tropical from "~/assets/audio/audio-tropical.mp3"
-import underwater from "~/assets/audio/audio-underwater.mp3"
 
 interface Track {
   type: string
@@ -18,22 +10,12 @@ interface Track {
 
 const store = useAuthStore()
 const token = store.getToken
-
-const audioTracks: Track[] = [
-  { type: "fire", id: "fireplace", src: fireplace },
-  { type: "forest", id: "forestriver", src: forestriver },
-  { type: "night", id: "nightambience", src: nightambience },
-  { type: "ocean", id: "ocean", src: ocean },
-  { type: "rain", id: "softrain", src: softrain },
-  { type: "thuder", id: "thunderstorm", src: thunderstorm },
-  { type: "tropical", id: "tropical", src: tropical },
-  { type: "underwater", id: "underwater", src: underwater },
-]
+const audioTracks = useAudioTracks()
 
 const audioPlaying = ref(false)
 const audio: any = ref(null)
 
-const playSound = (trackId: any) => {
+const playSound = (trackId: string) => {
   if (!audioPlaying.value) {
     audio.value = new Audio(trackId)
     audio.value.play()
