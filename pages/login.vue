f9efbc7634e790f739449ba1974a3942e3c0647b
frinica <you@example.com>
2023-07-18 17:12:51 +0200

    update styling of login page

--- pages/login.vue
+++ pages/login.vue
@@ -21,33 +21,39 @@ const login = async () => {
 </script>
 
 <template>
-  <section class="text-center p-20 bg-hero-pattern bg-cover h-screen">
-    <h1 class="text-7xl mb-16 text-white drop-shadow-lg font-serif">Retreat</h1>
+  <section class="text-center bg-hero-pattern py-20 bg-cover h-screen">
+    <div
+      class="bg-green-dark bg-opacity-50 mx-10 px-8 py-10 rounded-lg shadow-lg"
+    >
+      <h1 class="text-7xl mb-16 text-white drop-shadow-lg font-serif">
+        Retreat
+      </h1>
 
-    <form @submit.prevent="login" class="flex flex-col">
-      <input
-        v-model="user.email"
-        type="text"
-        name="email"
-        placeholder="Email"
-        required
-        class="border border-green rounded-lg px-2 py-1 mb-4 drop-shadow-lg opacity-80 focus:outline-green md:self-center md:w-[33%]"
-      />
-      <input
-        v-model="user.password"
-        type="password"
-        name="password"
-        placeholder="Password"
-        required
-        class="border border-green rounded-lg px-2 py-1 mb-8 drop-shadow-lg opacity-80 focus:outline-green md:self-center md:w-[33%]"
-      />
-      <div class="text-center">
-        <button
-          class="text-white bg-green py-2 px-6 rounded-xl border border-green-light drop-shadow-lg shadow-inner hover:bg-green-dark hover:border-green"
-        >
-          Sign in
-        </button>
-      </div>
-    </form>
+      <form @submit.prevent="login" class="flex flex-col">
+        <input
+          v-model="user.email"
+          type="text"
+          name="email"
+          placeholder="Email"
+          required
+          class="border border-green rounded-lg px-2 py-1 mb-4 drop-shadow-lg opacity-80 focus:outline-green md:self-center md:w-[33%] text-gray"
+        />
+        <input
+          v-model="user.password"
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          class="border border-green rounded-lg px-2 py-1 mb-8 drop-shadow-lg opacity-80 focus:outline-green md:self-center md:w-[33%] text-gray"
+        />
+        <div class="text-center">
+          <button
+            class="text-white bg-green py-2 px-6 rounded-xl border border-green-light drop-shadow-lg shadow-inner hover:bg-green-dark hover:border-green"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
   </section>
 </template>
